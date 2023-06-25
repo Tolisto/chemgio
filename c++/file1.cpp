d5de52e1735a3c248736e23ac5b750d705a3c977
Jacky <>
2023-06-26 00:03:28 +0700

    no message

---
+++ c++/file1.cpp
@@ -0,0 +1,98 @@
+//
+//  main.cpp
+//  cplus1
+//
+//  Created by User on 24/10/2021.
+//
+
+#include <iostream>
+//#include "quick_sort.hpp"
+//#include "merge_sort.hpp"
+//#include "graph/lienthong.hpp"
+//#include "file/docghifile.hpp"
+//#include "sinhnhiphan.hpp"
+//#include "sinhhoanvi.hpp"
+
+//#include "sizeof/sizeofclass.hpp"
+
+//#include "pointer/pointer.hpp"
+//
+//#include "friends/Friends.hpp"
+//
+//#include "argorithm/Fibonaci.hpp"
+//#include "argorithm/TinhToan.hpp"
+//
+//#include "argorithm/BaCaiTui.hpp"
+
+#include "dynamic/dp.hpp"
+
+int main(int argc, const char * argv[]) {
+//    int a[] = {13,2,6,10,456,23,445,1,4,2};
+//    int n = sizeof(a) / sizeof(a[0]);
+//    std::cout<<"Array: ";
+//    print(a, n);
+//    std::cout<<"Quick sort"<<std::endl;
+//    quick(a, 0, n - 1);
+//    std::cout<<std::endl<<"Merge sort"<<std::endl;
+//    merge(a, 0, n-1);
+//    print(a, n);
+//    std::cout<<std::endl;
+    
+//    LienThong lt;
+//    lt.readFile_LT("graph/input.txt");
+//    docfile("file/example.txt");
+//    ghifile("\nCon gi nua dau ha anh\nTam biet nhe!!!");
+    
+//    SNP snp(5);
+//    snp.khudequy();
+    
+//    SHV shv(5);
+//    shv.sinhHoanvi();
+//    shv.tim(0);
+    
+//    A a;
+//    std::cout<<sizeof(a)<<std::endl;
+//    std::cout<<sizeof(A)<<std::endl;
+//    a.getSizeOf();
+    
+//    Pointer p;
+//    p.show_array_pointer();
+//    p.maxArrayValue();
+    
+    // friend function
+    
+//    B b;
+//    initA(b, 16);
+//    b.display();
+    
+//    Fibo f(10);
+//    f.calculate();
+    
+//    TinhToan t;
+//    t.sodoixung();
+//    t.sodoixung2();
+//    t.giaithua();
+//    t.soAmstrong();
+//    t.swapNoParam();
+//    t.decimalToBinary();
+//    t.triangleAlphabet();
+//    t.triangleNumber();
+//    t.triangleFreeSpace();
+//    t.charToString();
+    
+    
+//    BCT btc;
+//    btc.init();
+//    btc.snp(0);
+//    btc.result();
+    
+//    fibonaci(50);
+    dongxuNN();
+    
+//    caitui();
+//    TongBangS();
+//    DayTangDaiNhatLIS();
+//    XauChungDaiNhatLCS();
+    XauDoiXungDaiNhatLPS();
+    return 0;
+}
